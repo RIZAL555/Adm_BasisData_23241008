4de2f3404622ecc099a55ec6f6510e549a2e3bde
RIZAL555 <147357342+RIZAL555@users.noreply.github.com>
2025-04-14 10:01:44 +0700

    Add files via upload

---
+++ Pre-Praktek/Pre-Dua (1).sql
@@ -0,0 +1,18 @@
+-- Nama : M.Afrizal
+-- Nim : 23241008
+-- Kekas : A
+
+-- Melihat Database dalam Mysql
+SHOW DATABASES;
+
+-- Melihat databases yang digunakan
+USE mysql;
+
+-- Menampilkan tabel dalam database mysql
+SHOW TABLES;
+
+-- Mendeskripsikan tabel user
+DESCRIBE user;
+
+-- Menampilkan isi data dari dalam kolom Host, User, Passwoard
+SELECT Host, User, Password FROM user;
